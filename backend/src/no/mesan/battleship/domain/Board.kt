16cd347b19c2ee3291dfdedfb4b1114ddb390671
Marius Strandhaug <mariuss@mesan.no>
2016-02-11 18:40:56 +0100

    Startet på newgame

--- backend/src/no/mesan/battleship/domain/Board.kt
+++ backend/src/no/mesan/battleship/domain/Board.kt
@@ -1,7 +1,39 @@
 package no.mesan.battleship.domain
 
-data class Board(val board: Array<IntArray>) {
-    companion object {
-        fun empty(size: Int) = Board(Array(size, { IntArray(size, { 0 }) }))
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Cell(val isHit: Boolean = false, val isOccupied: Boolean = false) {
+
+    fun hit() = if (isHit) this else Cell(true, isOccupied)
+
+}
+
+data class Board(val board: Array<Array<Cell>>) {
+    init {
+        val size = if (board.size > 0) board[0].size else 0
+        for (arr in board) {
+            if (arr.size != size) {
+                throw IllegalArgumentException("Board is not rectangular.")
+            }
+        }
     }
+
+    companion object {
+        fun empty(size: Int) = Board(Array(size, { Array(size, { Cell() }) }))
+    }
+
+}
+
+data class Coordinate(@JsonProperty(value = "first") val first: Int, @JsonProperty(value = "second") val second: Int)
+
+data class Ship(@JsonProperty(value = "start") val start: Coordinate, @JsonProperty(value = "end") val end: Coordinate) {
+
+    init {
+        if ((start.first != end.first && start.second != end.second)) {
+            throw IllegalArgumentException("Ship must be placed either horizontally or vertically.")
+        }
+    }
+
+    fun length() = Math.abs(end.first - start.first) + Math.abs(end.second - start.second) + 1
+
 }
--- backend/src/no/mesan/battleship/endpoint/BattleshipRestController.kt
+++ backend/src/no/mesan/battleship/endpoint/BattleshipRestController.kt
@@ -1,9 +1,8 @@
 package no.mesan.battleship.endpoint
 
 import no.mesan.battleship.domain.Board
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import no.mesan.battleship.domain.Ship
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class BattleshipRestController {
@@ -11,6 +10,18 @@ class BattleshipRestController {
     @RequestMapping("/emptyBoard")
     fun emptyBoard(@RequestParam(value = "size", defaultValue = "10") size: Int): Board {
         return Board.empty(size)
+    }
+
+    @RequestMapping(
+            consumes = arrayOf("application/json"),
+            method = arrayOf(RequestMethod.POST),
+            value = "/newGame")
+    fun newGame(@RequestParam(value = "username", required = false) username: String?,
+                @RequestBody ships: List<Ship>): String {
+
+        println(ships)
+
+        return username ?: "Hei"
     }
 
 }
