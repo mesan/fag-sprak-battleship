46d4f000597a4055f793cbe3471ac266779ddf35
Marius Strandhaug <mariuss@mesan.no>
2016-03-08 18:18:14 +0100

    Endret port

--- backend/src/no/mesan/battleship/Battleship.kt
+++ backend/src/no/mesan/battleship/Battleship.kt
@@ -1,13 +1,16 @@
 package no.mesan.battleship
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 
 @SpringBootApplication
 open class Application {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
+            SpringApplicationBuilder(Application::class.java)
+                    .properties(mapOf(Pair("server.port", 83746)))
+                    .build()
+                    .run(*args)
         }
     }
 }
