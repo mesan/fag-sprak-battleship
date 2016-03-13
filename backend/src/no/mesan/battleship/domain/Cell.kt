0ce1c0d0b15c973f3db9c096aac479470b3c17b8
Marius Strandhaug <mariuss@mesan.no>
2016-03-13 14:33:38 +0100

    La til shipId på Cell.

--- backend/src/no/mesan/battleship/domain/Cell.kt
+++ backend/src/no/mesan/battleship/domain/Cell.kt
@@ -1,7 +1,10 @@
 package no.mesan.battleship.domain
 
-data class Cell(val id: Int, val isHit: Boolean = false, val isOccupied: Boolean = false) {
+data class Cell(val id: Int, val shipId: Int? = null, val isHit: Boolean = false) {
 
-    fun hit() = if (isHit) this else Cell(id, true, isOccupied)
+    val isOccupied: Boolean
+        get() = shipId != null
+
+    fun hit() = if (isHit) this else Cell(id, shipId, true)
 
 }
